8aaf6a2d6c192440996f6ae40ca1cf275a765971
LAmena-dev <amena2laremm@gmail.com>
2025-11-18 14:54:41 +0800

    javascript 06 - 11 solved

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isYearDivBy4 = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivBy400 = year % 400 === 0;
 
+  if (isYearDivBy4 && (!isCentury || isYearDivBy400)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
