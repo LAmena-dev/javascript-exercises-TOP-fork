a1ad3e71f76e3aa90f7c9d8ae03c1c263a1f1ab9
LAmena-dev <amena2laremm@gmail.com>
2025-11-22 00:00:18 +0800

    javascript exercises done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumeric.includes(character))
+    .join("");
+
+  const reversed = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversed;
 };
 
 // Do not edit below this line
