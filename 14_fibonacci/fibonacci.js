03ab292b845bc26917d0ab0233724bdb6e8daa71
LAmena-dev <amena2laremm@gmail.com>
2025-11-22 08:07:17 +0800

    javascript exercises done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (fib) {
+  let prev = 0;
+  let current = 1;
 
+  if (typeof fib !== "number") {
+    fib = parseInt(fib);
+  }
+
+  if (fib < 0) return "OOPS";
+  if (fib == 0) return 0;
+
+  for (let i = 2; i <= fib; i++) {
+    let sum = prev + current;
+    prev = current;
+    current = sum;
+  }
+  return current;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const calcAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = calcAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = calcAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
